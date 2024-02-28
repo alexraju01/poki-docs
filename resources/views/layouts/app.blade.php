146c9ac9a0d98d827c0297b00b0cafcafb80ed70
CodeExcalibar <Alex.raju1010@gmail.com>
2024-02-28 13:58:05 +0000

    removed pokeType.css instead added pokeTypes.scss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pokémon Details</title>
-    <link rel="stylesheet" href="{{ asset('css/pokemon-types.css') }}">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+    @vite("resources/scss/pokeTypes.scss")
+    
     {{-- @stack('styles') --}}
 </head>
 <body>
--- resources/views/pokemons/index.blade.php
+++ resources/views/pokemons/index.blade.php
@@ -1,12 +1,14 @@
 
+@extends('layouts.app')
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pokemon</title>
-    <link rel="stylesheet" href="{{ asset('css/pokemon-types.css') }}">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    
 </head>
 <body>
     <h1>Pokemon List</h1>
