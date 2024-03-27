9874bb195864bf9ceaae0efa8956e8fb66de6e7f
amifidele <amifidele333@gmail.com>
2024-03-27 22:12:29 +0200

    update

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Services\PokemonApiServices;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Str;
 
 class PokemonController extends Controller
@@ -18,50 +19,58 @@ class PokemonController extends Controller
     }
 
     public function index(){
-    $limit=1000;
-    $pokemons = $this->pokemonApiService->fetchPokemons($limit);
-    return view('pokemons.index', ['pokemons' => $pokemons,'limit' => $limit]);
-}
+        $limit=1000;
+
+        $pokemons = Cache::remember('pokemons', now()->addDays(90), function () use ($limit) {
+            return $this->pokemonApiService->fetchPokemons($limit);
+        });
+
+        return view('pokemons.index', ['pokemons' => $pokemons,'limit' => $limit]);
+    }
 
 
     public function show($id)
-{
-    $basicInfo = $this->pokemonApiService->fetchPokemonData($id);
-    // dd($basicInfo['moves']);
-    $types = collect($basicInfo['types'])->pluck('type.name')->all();
-    $speciesInfo = Http::get($basicInfo['species']['url'])->json();
-    $genusType = collect($speciesInfo['genera'] ?? [])->firstWhere('language.name', 'en')['genus'] ?? null;
-   
-    $statsBarWithColor = $this->pokemonApiService->percentageStatsBarWithColor($basicInfo['stats'], $types[0]);
-    $strengthAndWeakness = $this->pokemonApiService->pokemonStrengthAndWeakness($id);
+    {
+        // Define a unique cache key based on the Pokemon ID
+        $cacheKey = 'pokemon_' . $id;
 
-    // Select an English description (flavor text) from a specific version for consistency
-    $englishDescription = collect($speciesInfo['flavor_text_entries'])
-    ->where('language.name', 'en')
-    ->take(4)
-    ->map(function ($entry) {
-    return Str::of($entry['flavor_text'])->replace(["\n", "\f", "\r"], " ");})
-    ->unique()
-    ->implode(' ');
+        // Attempt to retrieve the data from cache
+        $pokemonInfo = Cache::remember($cacheKey, now()->addDays(90), function () use ($id) {
+            // If not found in cache, fetch data from the API
+            $basicInfo = $this->pokemonApiService->fetchPokemonData($id);
+            $types = collect($basicInfo['types'])->pluck('type.name')->all();
+            $speciesInfo = Http::get($basicInfo['species']['url'])->json();
+            $genusType = collect($speciesInfo['genera'] ?? [])->firstWhere('language.name', 'en')['genus'] ?? null;
+            $statsBarWithColor = $this->pokemonApiService->percentageStatsBarWithColor($basicInfo['stats'], $types[0]);
+            $strengthAndWeakness = $this->pokemonApiService->pokemonStrengthAndWeakness($id);
 
-    $pokemonInfo = [
-        'id' => $basicInfo['id'],
-        'name' => $basicInfo['name'],
-        'sprite' => $basicInfo['sprites']['other']['official-artwork']['front_default'],
-        'types' => $types,
-        'genus' => $genusType,
-        'stats' => $statsBarWithColor,    
-        'ability' => $this->pokemonApiService->fetchPokemonAbilities($basicInfo['abilities']),  
-        'moves' => $this->pokemonApiService->fetchPokemonMoves($basicInfo['moves']), 
-        'strengths' => $strengthAndWeakness['strengths'],   
-        'weaknesses' => $strengthAndWeakness['weaknesses'],
-        'description' => $englishDescription,
-        'evolutions' => $this->pokemonApiService->showEvolutions($basicInfo['name'])               
+            $englishDescription = collect($speciesInfo['flavor_text_entries'])
+                ->where('language.name', 'en')
+                ->take(4)
+                ->map(function ($entry) {
+                    return Str::of($entry['flavor_text'])->replace(["\n", "\f", "\r"], " ");
+                })
+                ->unique()
+                ->implode(' ');
 
-    ];
-    
-    return view('pokemons.show',compact('pokemonInfo'));
-}
+            return [
+                'id' => $basicInfo['id'],
+                'name' => $basicInfo['name'],
+                'sprite' => $basicInfo['sprites']['other']['official-artwork']['front_default'],
+                'types' => $types,
+                'genus' => $genusType,
+                'stats' => $statsBarWithColor,
+                'ability' => $this->pokemonApiService->fetchPokemonAbilities($basicInfo['abilities']),
+                'moves' => $this->pokemonApiService->fetchPokemonMoves($basicInfo['moves']),
+                'strengths' => $strengthAndWeakness['strengths'],
+                'weaknesses' => $strengthAndWeakness['weaknesses'],
+                'description' => $englishDescription,
+                'evolutions' => $this->pokemonApiService->showEvolutions($basicInfo['name'])
+            ];
+        });
+
+        return view('pokemons.show', compact('pokemonInfo'));
+    }
 
 
 }
