e3c78fcb9d591afd7f262c8a0efeb189ffa09437
CodeExcalibar <Alex.raju1010@gmail.com>
2024-04-29 18:47:46 +0100

    fixing task scheduling

--- routes/console.php
+++ routes/console.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
 // Schedule::command('app:cache-all-pokemon')->cron('0 0 */90 * *');
-// schedule::command('app:cache-all-pokemon')->everyMinute();
+Schedule::command('app:cache-all-pokemon')->everyMinute();
 
